ff0043e003b1b0d215feedc44409b341c47bcff7
“Sava” <savelmy@inbox.ru>
2023-04-05 14:59:48 +0700

    sis

--- python/1_05.04.23.py
+++ python/1_05.04.23.py
@@ -3,13 +3,13 @@ os.system('clear')
 
 def foo(a):
     a=[*a]
-    for i in range (len(a)):
+    for i in range(len(a)):
         if a[i] == 5:
             a[i] = 3
             
-        return a 
+    return a 
 
-test = [5, 5, 5, 5, 5]
+test = [1, 2, 3, 4, 5]
 
 res = foo(test)
 
